ace354cf29cb9e81855cebcebe1519402af98811
subrath <subrathitachi6@gmail.com>
2022-11-14 08:57:37 +0530

    fixed bugs

--- base-package-install.sh
+++ base-package-install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt install build-essential imwheel dmenu curl pulseaudio lxappearance gnome-key policykit-1-gnome libghc-gi-harfbuzz-dev libxft-dev openbox zsh zsh-autosuggestions zsh-syntax-highlighting git chromium openjdk-17-jdk network-manager wget unzip thunar feh
+sudo apt install build-essential imwheel dmenu curl pulseaudio lxappearance gnome-keyring policykit-1-gnome libghc-gi-harfbuzz-dev libxft-dev openbox zsh zsh-autosuggestions zsh-syntax-highlighting git chromium openjdk-17-jdk network-manager wget unzip thunar feh -y
 wget https://github.com/neovim/neovim/releases/download/v0.8.0/nvim-linux64.deb
 sudo dpkg -i nvim-linux64.deb
 wget https://downloads.gradle-dn.com/distributions/gradle-7.5.1-bin.zip
